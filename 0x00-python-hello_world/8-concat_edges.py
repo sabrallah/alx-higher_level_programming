b9c62b4f3a85e965de440c6f9988929f7de0715b
sabrallah <sabrallah1991@gmail.com>
2023-06-05 06:55:26 -0700

    task8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming
+language that combines remarkable power with very clear syntax"
+print(str[43:80] + str[107:112] + str[0:6] + str[6:9] + str[12:20] + str[20:28] + str[30:43] + str[80:107])
