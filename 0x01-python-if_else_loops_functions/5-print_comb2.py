857098acbacb58da34e371669de6408f3d90eeb6
sabrallah <sabrallah1991@gmail.com>
2023-06-06 06:07:10 -0700

    task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if i == 9 and j == 9:
-            print("{:d}{:d}".format(i, j))
+def print_numbers():
+    for i in range(100):
+        if i < 99:
+            print("{:02d}, ".format(i), end="")
         else:
-            print("{:d}{:d}, ".format(i, j), end="")
+            print("{:02d}".format(i))
 
+if __name__ == "__main__":
+    print_numbers()
