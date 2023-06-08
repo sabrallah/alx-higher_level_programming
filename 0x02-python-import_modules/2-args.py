d1c8cd0309f30ca5e0b036f5f68a55a32d0a4d61
sabrallah <sabrallah1991@gmail.com>
2023-06-08 05:55:24 -0700

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:] # get the arguments without the script name
+    n = len(argv) # get the number of arguments
+
+    if n == 0:
+        print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(n))
+
+    for i, arg in enumerate(argv, 1): # loop through the arguments with their indices
+        print("{}: {}".format(i, arg)) # print the position and the value of each argument
