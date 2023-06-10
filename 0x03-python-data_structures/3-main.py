a209c8df4847943f02942972a6de04711abbd6a2
sabrallah <sabrallah1991@gmail.com>
2023-06-10 05:21:24 -0700

    task3

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-# Import the function from the module
-from 3-print_reversed_list_integer import print_reversed_list_integer
+print_reversed_list_integer = \
+    __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-# Define the list
 my_list = [1, 2, 3, 4, 5]
-
-# Call the function with the list as argument
 print_reversed_list_integer(my_list)
