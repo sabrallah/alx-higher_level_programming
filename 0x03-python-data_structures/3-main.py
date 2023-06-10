8771ab1e6152492c2dc4d2d75bf0fc854aae0881
sabrallah <sabrallah1991@gmail.com>
2023-06-10 05:18:18 -0700

    task3

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+# Import the function from the module
+from 3-print_reversed_list_integer import print_reversed_list_integer
 
+# Define the list
 my_list = [1, 2, 3, 4, 5]
+
+# Call the function with the list as argument
 print_reversed_list_integer(my_list)
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-  for i in range(len(my_list) - 1, -1, -1):
-    print(str.format("{0}", my_list[i]))
+    # loop through the list from the last element to the first
+    for y in range(len(my_list) - 1, -1, -1):
+        # print the current element using str.format()
+        print("{:d}".format(my_list[y]))
