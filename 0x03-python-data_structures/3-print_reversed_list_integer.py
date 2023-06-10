38d24abc2f987900d2d22c4025293d98c855d42c
sabrallah <sabrallah1991@gmail.com>
2023-06-10 05:02:16 -0700

    task3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    # loop through the list items in reverse order
-    for number in reversed(my_list):
-        # print each number using str.format()
-        print("{:d}".format(number))
