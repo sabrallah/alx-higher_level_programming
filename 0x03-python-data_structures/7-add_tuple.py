b7c127ebd994fe23b74f497de710f9bb4283b57b
sabrallah <sabrallah1991@gmail.com>
2023-06-10 06:00:20 -0700

    task7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Initialize the result tuple with zeros
-    result = (0, 0)
-    # Loop through the first two elements of each tuple
-    for i in range(2):
-        # Add the corresponding elements of the tuples if they exist
-        if i < len(tuple_a):
-            result = (result[0] + tuple_a[i], result[1])
-        if i < len(tuple_b):
-            result = (result[0], result[1] + tuple_b[i])
-    # Return the result tuple
-    return result
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    new_tuple = (a[0] + b[0], a[1] + b[1])
+    return new_tuple
