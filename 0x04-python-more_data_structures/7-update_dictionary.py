385ba562c1cc8b1c9771e610cecf2b92bd3e0ef6
sabrallah <sabrallah1991@gmail.com>
2023-06-14 03:14:35 -0700

    task7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    # use the dictionary assignment syntax to add or replace the key/value pair
+    a_dictionary[key] = value
+    # return the updated dictionary
+    return a_dictionary
