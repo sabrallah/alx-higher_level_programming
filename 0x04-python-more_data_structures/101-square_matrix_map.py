f7d6eb4143c55830152825770195a4b2553d0b7a
sabrallah <sabrallah1991@gmail.com>
2023-06-14 04:32:10 -0700

    task14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -2,4 +2,4 @@
 
 def square_matrix_map(matrix=[]):
     # use map to apply a lambda function that squares each element of each row
-    return (list(map(lambda row: list(map(lambda y: y ** 2, row)), matrix)))
+    return (list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix)))
