df3ec6ad8f8ad7de6f372e985b29ef0bd3ff6090
sabrallah <sabrallah1991@gmail.com>
2023-06-14 04:03:11 -0700

    task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,38 +1,26 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-
-    """Converts a Roman numeral to an integer.
-
-    Args:
-        roman_string: A string representing a Roman numeral.
-
-    Returns:
-        The integer value of the Roman numeral.
-
-    Raises:
-        ValueError: If the roman_string is not a valid Roman numeral.
-    """
-
-    if not isinstance(roman_string, str):
+    # check if the roman_string is a string and not None
+    if not isinstance(roman_string, str) or roman_string is None:
+        # return 0
         return 0
-
-    roman_numerals = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000,
-    }
-
-    value = 0
+    # create a dictionary to store the values of each roman numeral
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    # initialize a variable to store the result
+    result = 0
+    # loop through the characters of the roman_string from left to right
     for i in range(len(roman_string)):
-        if roman_numerals[roman_string[i]] > \
-                roman_numerals[roman_string[i + 1]]:
-            value -= roman_numerals[roman_string[i]]
+        # get the value of the current character
+        current = roman_values[roman_string[i]]
+        # check if we are not at the last character
+        # and the next character is larger than the current one
+        if i < len(roman_string) - 1 \
+                and roman_values[roman_string[i + 1]] > current:
+            # subtract the current value from the result
+            result -= current
         else:
-            value += roman_numerals[roman_string[i]]
-
-    return value
+            # add the current value to the result
+            result += current
+    # return the result
+    return result
