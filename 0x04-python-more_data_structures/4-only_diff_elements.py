6ab2315c1721970dd4da0fca79c551e91aa92008
sabrallah <sabrallah1991@gmail.com>
2023-06-14 03:00:16 -0700

    task4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    """use the set symmetric difference
+    method to find the elements present in only one set"""
+    return set_1.symmetric_difference(set_2)
