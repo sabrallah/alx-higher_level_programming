a3f300d6628cccb95e2090ba80e42e3f80eed915
sabrallah <sabrallah1991@gmail.com>
2023-06-14 05:48:02 -0700

    task16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -5,9 +5,9 @@
 
 void print_python_bytes(PyObject *p)
 {
-	long int volum;
-	int y;
-	char *my_str = NULL;
+	long int size;
+	int i;
+	char *trying_str = NULL;
 
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
@@ -16,34 +16,34 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	PyBytes_AsStringAndSize(p, &my_str, &volum);
+	PyBytes_AsStringAndSize(p, &trying_str, &size);
 
-	printf("  volum: %li\n", volum);
-	printf("  trying string: %s\n", my_str);
-	if (volum < 10)
-		printf("  first %li bytes:", volum + 1);
+	printf("  size: %li\n", size);
+	printf("  trying string: %s\n", trying_str);
+	if (size < 10)
+		printf("  first %li bytes:", size + 1);
 	else
 		printf("  first 10 bytes:");
-	for (y = 0; y <= volum && y < 10; y++)
-		printf(" %02hhx", my_str[y]);
+	for (i = 0; i <= size && i < 10; i++)
+		printf(" %02hhx", trying_str[i]);
 	printf("\n");
 }
 
 void print_python_list(PyObject *p)
 {
-        long int volum = PyList_Size(p);
-        int y;
+        long int size = PyList_Size(p);
+        int i;
         PyListObject *list = (PyListObject *)p;
         const char *type;
 
         printf("[*] Python list info\n");
-        printf("[*] volum of the Python List = %li\n", volum);
+        printf("[*] Size of the Python List = %li\n", size);
         printf("[*] Allocated = %li\n", list->allocated);
-        for (y = 0; y < volum; y++)
+        for (i = 0; i < size; i++)
         {
-                type = (list->ob_item[y])->ob_type->tp_name;
-		printf("Element %y: %s\n", y, type);
+                type = (list->ob_item[i])->ob_type->tp_name;
+		printf("Element %i: %s\n", i, type);
                 if (!strcmp(type, "bytes"))
-                        print_python_bytes(list->ob_item[y]);
+                        print_python_bytes(list->ob_item[i]);
         }
 }
