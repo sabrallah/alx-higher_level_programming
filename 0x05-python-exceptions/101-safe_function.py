4c3d216633e42346a824e09acef42825564dc3c4
sabrallah <sabrallah1991@gmail.com>
2023-06-26 07:25:52 -0700

    task8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        import sys
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
