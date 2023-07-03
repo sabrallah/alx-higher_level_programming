4c383b2e5c1250b3230c02bcd55a36e055dee5bc
sabrallah <sabrallah1991@gmail.com>
2023-07-03 04:39:59 -0700

    task10

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+Solves the N-queens problem using backtracking.
+"""
+
+import sys
+
+
+def is_safe(board, row, col, N):
+    """Determines if it's safe to place a queen at a given position."""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens_util(board, col, N):
+    """Solves the N-queens problem using backtracking."""
+    if col == N:
+        solution = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        print(solution)
+        return
+
+    for i in range(N):
+        if is_safe(board, i, col, N):
+            board[i][col] = 1
+            solve_nqueens_util(board, col + 1, N)
+            board[i][col] = 0
+
+
+def solve_nqueens(N):
+    """Solves the N-queens problem."""
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solve_nqueens_util(board, 0, N)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solve_nqueens(N)
