14705ad3f5b2974c7bbeb89248dff6bc817bd09a
sabrallah <sabrallah1991@gmail.com>
2023-07-04 05:18:47 -0700

    task19

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+This module contains a function to create a copy of a list.
+"""
 
 def copy_list(l):
+    """
+    Create a copy of a list.
+
+    Args:
+        l (list): The list to be copied.
+
+    Returns:
+        list: A copy of the input list.
+    """
     return l[:]
