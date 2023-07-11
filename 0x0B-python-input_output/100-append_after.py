384ef18f6f150cf85b9c0be1868af501d1798211
sabrallah <sabrallah1991@gmail.com>
2023-07-11 07:37:12 -0700

    task13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,14 +1,20 @@
+i want like this code : 
 #!/usr/bin/python3
-def append_after(filename="", search_string="", new_string=""):
-    """
-    Inserts a line of text to a file,
-    after each line containing a specific string
-    """
-    with open(filename, 'r') as f:
-        lines = f.readlines()
+"""
+Module append after method.
+"""
 
-    with open(filename, 'w') as f:
-        for line in lines:
-            f.write(line)
-            if search_string in line:
-                f.write(new_string)
+
+def append_after(filename="", search_string="", new_string=""):
+    '''method inserting text after search string.'''
+    lines = []
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        y = 0
+        while y < len(lines):
+            if search_string in lines[y]:
+                lines[y:y + 1] = [lines[y], new_string]
+                y += 1
+            y += 1
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(lines)
