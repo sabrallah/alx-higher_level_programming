41a1db133ff0b6d4485413e12c018c9403482d30
sabrallah <sabrallah1991@gmail.com>
2023-07-11 04:47:26 -0700

    task2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A module containing the 'append_write' function."""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file (UTF8) and
+    returns the number of characters added.
+
+    Args:
+        filename (str): The name of the file to append text to.
+        text (str): The text to append to the file.
+
+    Returns:
+        int: The number of characters added.
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        return file.write(text)
