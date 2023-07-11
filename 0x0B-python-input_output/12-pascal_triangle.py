3677ec33554565ae60ff171cd0b33b131f12249e
sabrallah <sabrallah1991@gmail.com>
2023-07-11 07:18:38 -0700

    task12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,7 +6,8 @@ This module contains a function that generates Pascal's triangle.
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal’s triangle of n.
+    Returns a list of lists of integers
+    representing the Pascal’s triangle of n.
     Returns an empty list if n <= 0.
     """
     if n <= 0:
