f99711aa5a32e4fb28a4a966ff4a5002910f3609
sabrallah <sabrallah1991@gmail.com>
2023-07-11 07:10:15 -0700

    task11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module defines a Student class with public instance attributes and methods.
+This module defines a Student class
+with public instance attributes and methods.
 """
 
 
@@ -8,7 +9,8 @@ class Student:
     """A class representing a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a Student instance with first_name, last_name, and age."""
+        """Initializes a Student instance
+        with first_name, last_name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -16,7 +18,8 @@ class Student:
     def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of a Student instance.
-        If attrs is a list of strings, only attributes names contained in this list are retrieved.
+        If attrs is a list of strings,
+        only attributes names contained in this list are retrieved.
         Otherwise, all attributes are retrieved.
         """
         if attrs is not None and isinstance(attrs, list):
