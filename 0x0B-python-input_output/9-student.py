2907911fb942cb05356a66f7f912e2dbe401c933
sabrallah <sabrallah1991@gmail.com>
2023-07-11 06:02:16 -0700

    task9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -14,7 +14,7 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """
-        Initializes a Student instance with given first_name, last_name, and age.
+        Initializes a Student instance with given first_name, last_name, age.
         """
         self.first_name = first_name
         self.last_name = last_name
