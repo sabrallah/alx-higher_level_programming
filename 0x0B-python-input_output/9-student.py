d48df7148bf639fd923ec814173840496a495ec6
sabrallah <sabrallah1991@gmail.com>
2023-07-11 06:06:36 -0700

    task9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """
-This module contains a Student class that defines a student with
-first_name, last_name, and age attributes, and a method to retrieve
-a dictionary representation of the student instance.
+This module contains a Student class that defines a student by their
+first_name, last_name, and age, and includes a method to retrieve a
+dictionary representation of the instance.
 """
 
 
 class Student:
     """
-    Student class with public instance attributes and a method to
-    retrieve a dictionary representation of the student instance.
+    Student class that defines a student with first_name, last_name, and age.
+    Includes a method to retrieve a dictionary representation of the instance.
     """
 
     def __init__(self, first_name, last_name, age):
         """
-        Initializes a Student instance with given first_name, last_name, age.
+        Initializes a new Student instance with the given first_name,
+        last_name, and age.
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -22,7 +23,8 @@ class Student:
 
     def to_json(self):
         """
-        Retrieves a dictionary representation of the Student instance.
+        Retrieves a dictionary representation of a Student instance, which can
+        be used for JSON serialization.
         """
         return self.__dict__
 
@@ -39,3 +41,4 @@ if __name__ == "__main__":
         print(type(j_student['first_name']))
         print(j_student['age'])
         print(type(j_student['age']))
+
