7d5dfd33f2660dc204ff01a07e564fc836cd50d7
sabrallah <sabrallah1991@gmail.com>
2023-07-11 06:11:53 -0700

    task9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,44 +1,17 @@
 #!/usr/bin/python3
 """
-This module contains a Student class that defines a student by their
-first_name, last_name, and age, and includes a method to retrieve a
-dictionary representation of the instance.
+The class "Student" container
 """
 
 
 class Student:
-    """
-    Student class that defines a student with first_name, last_name, and age.
-    Includes a method to retrieve a dictionary representation of the instance.
-    """
-
+    """represent of a student"""
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a new Student instance with the given first_name,
-        last_name, and age.
-        """
+        """initialize the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """
-        Retrieves a dictionary representation of a Student instance, which can
-        be used for JSON serialization.
-        """
+        """returns a dictionary representation of a Student instance"""
         return self.__dict__
-
-
-if __name__ == "__main__":
-    Student = __import__('9-student').Student
-
-    students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
-
-    for student in students:
-        j_student = student.to_json()
-        print(type(j_student))
-        print(j_student['first_name'])
-        print(type(j_student['first_name']))
-        print(j_student['age'])
-        print(type(j_student['age']))
-
