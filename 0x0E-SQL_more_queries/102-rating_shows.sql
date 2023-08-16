9d8ac7604be460955ba6726f1c5ac7d25fc293b4
sabrallah <sabrallah1991@gmail.com>
2023-08-16 10:10:28 -0700

    task19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,11 +1,11 @@
--- Lists all shows and their ratings from the database hbtn_0d_tvshows
--- Uses an INNER JOIN to match tv_shows and tv_show_ratings
+-- Lists all shows from the hbtn_0d_tvshows_rate database
+-- Joins the tv_shows and tv_show_ratings tables to match the show IDs
+-- Calculates the sum of the rates for each show to get its overall rating 
+-- Groups the results by show title to aggregate the ratings per show
+-- Orders the shows by their calculated rating in descending order
 
-SELECT 
-   tv_shows.title,
-   tv_show_ratings.rating
-FROM
-   tv_shows
-   INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-ORDER BY
-   tv_shows.title;
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id  
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
