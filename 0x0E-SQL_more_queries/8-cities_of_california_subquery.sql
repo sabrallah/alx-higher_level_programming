f04a383fcf0ce1613d3b747ea98a9bc46473da91
sabrallah <sabrallah1991@gmail.com>
2023-08-16 09:05:39 -0700

    task8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,8 @@
 -- List all cities contained in the database hbtn_0d_usa
-
-SELECT id, name 
-FROM hbtn_0d_usa.cities
+SELECT id, name
+FROM cities
 WHERE state_id = (
-    -- Get id of California state
-    SELECT id FROM hbtn_0d_usa.states
-    WHERE name = 'California'
+      SELECT id
+      FROM states
+      WHERE name = 'California'
 );
