ca9df9d0493c222b2c72507bde77e3ac2d8717de
sabrallah <sabrallah1991@gmail.com>
2023-08-16 05:42:22 -0700

    task0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- Lists privileges for user_0d_1 @localhost
+-- Lists privileges for user_0d_1 @localhos && user_0d_2 @localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- Lists privileges for user_0d_2 @localhost 
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
