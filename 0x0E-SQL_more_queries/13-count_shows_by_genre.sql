ff879da95b269ce10db154fda1e5f4bbbb0f0a99
sabrallah <sabrallah1991@gmail.com>
2023-08-16 09:28:49 -0700

    task13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,5 @@
 -- Select the genre and count the number of shows linked to each genre
+-- Select the genre and count the number of shows linked to each genre
 SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
