f68320c02cb3aebde1b472d4d5bd181e7e7be2da
sabrallah <sabrallah1991@gmail.com>
2023-08-16 10:04:40 -0700

    task18

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,17 @@
 -- Lists all shows without the genre Comedy
+-- This script lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
 
--- Lists all shows without the genre Comedy
-SELECT tv_shows.title 
-FROM tv_shows 
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id != 5
-ORDER BY tv_shows.title ASC;
+-- Use of inner join, to join more than 2 tables
+
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN (
+    SELECT tv_shows.title
+    FROM tv_shows
+    JOIN tv_show_genres
+    ON tv_show_genres.show_id = tv_shows.id
+    JOIN tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY tv_shows.title;
