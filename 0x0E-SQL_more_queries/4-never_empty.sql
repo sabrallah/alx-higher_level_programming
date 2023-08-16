0b4b86a79c8355b3f8ca24721a32bba4d4f45854
sabrallah <sabrallah1991@gmail.com>
2023-08-16 08:51:28 -0700

    task4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,9 +1,5 @@
--- creates the table id_not_null with a column id that will never be empty
-CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT DEFAULT 1 UNIQUE
-);
+-- Create table 'id_not_null'
+-- id INT default value 1, name VARCHAR(256)
+-- Should not fail if table exists
 
--- insert a row if table is empty
-INSERT INTO id_not_null (id)
-SELECT 1 FROM id_not_null
-WHERE NOT EXISTS (SELECT 1 FROM id_not_null);
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
