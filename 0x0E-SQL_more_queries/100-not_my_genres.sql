dc3547683a68038ea4019e354949e64399f268a2
sabrallah <sabrallah1991@gmail.com>
2023-08-16 09:58:32 -0700

    task17

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,14 +1,16 @@
--- Select the name column from the tv_genres table
--- Select the name column from the tv_genres table
+-- script uses hbtn_0d_tvshows database to
+-- list genres no linked to show 'Dexter'
+
+-- Use Join and subquery for geting required data
 SELECT tv_genres.name
--- Left join the tv_show_genres table on the genre_id column
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
--- Left join the tv_shows table on the show_id column
-LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
--- Filter the results to only include genres not linked to the show Dexter
-WHERE tv_shows.title != 'Dexter' OR tv_shows.title IS NULL
--- Group the results by the genre name
-GROUP BY tv_genres.name
--- Order the results in ascending order by the genre name
+WHERE tv_genres.name NOT IN (
+    SELECT tv_genres.name
+    FROM tv_genres
+    JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+    JOIN tv_shows
+    ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_shows.title = 'Dexter'
+)
 ORDER BY tv_genres.name;
