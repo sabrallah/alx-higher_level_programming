b83120d3877f97c8abc4cd9e83cdd191e70abd35
sabrallah <sabrallah1991@gmail.com>
2023-08-16 08:43:35 -0700

    task3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,19 +1,19 @@
 -- This script is used to force a specific name for a table in MySQL
 
 -- Drop the existing table if it exists
-DROP TABLE IF EXISTS `force_name`;
+DROP TABLE IF EXISTS 'force_name';
 
 -- Create the table with the desired name
-CREATE TABLE `force_name` (
-  `id` INT PRIMARY KEY,
-  `name` VARCHAR(50) NOT NULL
+CREATE TABLE 'force_name' (
+  'id' INT PRIMARY KEY,
+  'name' VARCHAR(50) NOT NULL
 );
 
 -- Insert some sample data into the table
-INSERT INTO `force_name` (`id`, `name`)
+INSERT INTO 'force_name' ('id', 'name')
 VALUES (1, 'John'),
        (2, 'Jane'),
        (3, 'Alice');
 
 -- Query the data from the table
-SELECT * FROM `force_name`;
+SELECT * FROM 'force_name';
