edb127ba7e71f940078e7959556c23514af410ce
sabrallah <sabrallah1991@gmail.com>
2023-08-16 05:39:32 -0700

    task0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- creates the user user_0d_1 with password user_0d_1_pwd 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; 
+-- Lists privileges for user_0d_1 @localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- grants SELECT privilege to user_0d_1 on database hbtn_0d_tvshows
-GRANT SELECT ON `hbtn_0d_tvshows`.* TO 'user_0d_1'@'localhost';
+-- Lists privileges for user_0d_2 @localhost 
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
