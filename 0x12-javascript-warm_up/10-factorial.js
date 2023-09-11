d25ce96cbeadd47139cd8a37d74b7ba1a209f155
sabrallah <sabrallah1991@gmail.com>
2023-09-11 04:06:36 -0700

    task10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 function factorial(n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+    if (!n || n < 2) {
+        return 1;
+    }
+    return n * factorial(n - 1);
 }
 
-console.log(factorial(Number(process.argv[2])));
+console.log(factorial(process.argv[2]));
