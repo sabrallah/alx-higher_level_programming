6ad351d37749b5fa87649d69c69a103bc49a7e40
sabrallah <sabrallah1991@gmail.com>
2023-09-11 04:22:10 -0700

    task11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+    console.log(0);
+} else {
+    let args = process.argv.slice(2).map(Number);
+    args.sort((a, b) => b - a);
+    console.log(args[1]);
+}
