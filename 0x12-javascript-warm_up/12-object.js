4b8690debf86485c45ddbeda70083e5bb4f9a9b8
sabrallah <sabrallah1991@gmail.com>
2023-09-11 04:31:09 -0700

    task12

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
+  value: 12
 };
 console.log(myObject);
-
 // Update the value of 'value' property
 myObject.value = 89;
-
 console.log(myObject);
