a8cfb85ff58508c7c3e0a3962691201b6673ad1a
sabrallah <sabrallah1991@gmail.com>
2023-09-11 05:12:30 -0700

    task17

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// this is my own code.
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  that.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
