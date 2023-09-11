25ac8c23ccc468fdca3c31c03a87c273eab6aab2
sabrallah <sabrallah1991@gmail.com>
2023-09-11 03:51:07 -0700

    task10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+'use strict';
+
+function factorial(n) {
+  if (!n || n < 2) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const arg = parseInt(process.argv[2]);
+console.log(factorial(arg));
