bcfaa14a8b0b79a45b7db5cbebc4489d4e88ebae
sabrallah <sabrallah1991@gmail.com>
2023-09-11 02:12:29 -0700

    task0

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-
-// Script that prints "JavaScript is amazing".
-
+// i write a code that log 'javascript is amazing'
 
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
