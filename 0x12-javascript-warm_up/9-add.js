16ca43aeb380ea852232a851609054c2144c9da3
sabrallah <sabrallah1991@gmail.com>
2023-09-11 03:36:37 -0700

    task9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+
+if (process.argv.length >= 4) {
+  console.log(add(process.argv[2], process.argv[3]));
+} else {
+  console.log('NaN');
+}
