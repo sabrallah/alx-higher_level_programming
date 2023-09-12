8a12b5ef830e960b414d82d51f061411617381a4
sabrallah <sabrallah1991@gmail.com>
2023-09-12 03:04:26 -0700

    task101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const { dict } = require('./101-data.js');
 
-let newDict = {};
-for (let key in dict) {
+const newDict = {};
+for (const key in dict) {
   if (newDict[dict[key]]) {
     newDict[dict[key]].push(key);
   } else {
