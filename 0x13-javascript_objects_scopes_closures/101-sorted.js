76238ea526ebfe6973c595b4c586d110d3aa5e37
sabrallah <sabrallah1991@gmail.com>
2023-09-12 03:03:09 -0700

    task101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { dict } = require('./101-data.js');
+
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]]) {
+    newDict[dict[key]].push(key);
+  } else {
+    newDict[dict[key]] = [key];
+  }
+}
+
+console.log(newDict);
