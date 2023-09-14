8c8b6cd317a55a05c8eea7d776a4d39be0a4cb32
sabrallah <sabrallah1991@gmail.com>
2023-09-14 04:49:14 -0700

    task2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This scripts take in an arguments and
+display all value in the state
+wheres `name` matche the arguments
+frome the databases `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Accesse to the databases and gets tha states
+    from the databases.
+    """
+
+    idb = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], idb=argv[3])
+
+    icur = idb.cursor()
+    icur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
+    irows = icur.fetchall()
+
+    for row in irows:
+        print(row)
