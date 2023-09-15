7f7364fefe42d41bb06b11aa9d4f43b08ca5571e
sabrallah <sabrallah1991@gmail.com>
2023-09-15 15:45:30 -0700

    task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,49 +1,28 @@
 #!/usr/bin/python3
-"""
-Script listing states starting with N from database
-"""
+'''
+list all states with an name start with N
+'''
 
 import MySQLdb
 import sys
 
-
-def main():
-
-    conn = connect_db()
-    cursor = conn.cursor()
-
-    query = "SELECT * FROM states WHERE name LIKE 'N%'"
-    execute_query(cursor, query)
-
-    states = fetch_all(cursor)
-    print_states(states)
-
-    close_resources(cursor, conn)
-
-
-def connect_db():
-    return MySQLdb.connect(
+if __name__ == '__main__':
+    db = MySQLdb.connect(
         user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         port=3306,
-        host='localhost'
-    )
+        host='localhost')
 
+    cursor = db.cursor()
+    cursor.execute("SELECT * \
+                    FROM states \
+                    WHERE CONVERT(`name` USING Latin1) \
+                    COLLATE Latin1_General_CS \
+                    LIKE 'N%';")
 
-def execute_query(cursor, query):
-    cursor.execute(query)
-
-
-def fetch_all(cursor):
-    return cursor.fetchall()
-
-
-def print_states(states):
+    states = cursor.fetchall()
     for state in states:
         print(state)
-
-
-def close_resources(cursor, conn):
     cursor.close()
-    conn.close()
+    db.close()
