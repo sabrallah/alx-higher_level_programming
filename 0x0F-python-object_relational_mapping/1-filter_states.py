34db88d92628b3dacd205b66a9ac3b18dcf14c63
sabrallah <sabrallah1991@gmail.com>
2023-09-15 15:28:44 -0700

    task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bi/python3
 """
-Script listing states starting with N from hbtn_0e_0_usa db
+Script listing states starting with N from 'hbtn_0e_0_usa' database
 """
 
 import MySQLdb
@@ -8,41 +8,53 @@ import sys
 
 
 def main():
+    """Main function"""
 
     connection = connect_to_db()
-    cursor = connection.cursor()
+    cursor = get_cursor(connection)
 
     # Query to select states starting with N
     query = ("SELECT * FROM states "
              "WHERE name LIKE 'N%'")
 
-    cursor.execute(query)
+    execute_query(cursor, query)
+    fetched_states = fetch_states(cursor)
 
-    states = cursor.fetchall()
+    print_states(fetched_states)
 
-    print_states(states)
-
-    cursor.close()
-    connection.close()
+    close_resources(cursor, connection)
 
 
 def connect_to_db():
     """Connect to MySQL database"""
-
     return MySQLdb.connect(
         user=sys.argv[1],
         password=sys.argv[2],
         database=sys.argv[3],
         port=3306,
-        host='localhost'
-    )
+        host='localhost')
+
+
+def get_cursor(connection):
+    return connection.cursor()
+
+
+def execute_query(cursor, query):
+    cursor.execute(query)
+
+
+def fetch_states(cursor):
+    return cursor.fetchall()
 
 
 def print_states(states):
-    """Print fetched states"""
-
     for state in states:
         print(state)
+
+
+def close_resources(cursor, connection):
+    cursor.close()
+    connection.close()
 
 
 if __name__ == '__main__':
