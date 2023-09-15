feb836ff760a018281da33561fc9d6b5a08b2197
sabrallah <sabrallah1991@gmail.com>
2023-09-15 16:08:35 -0700

    task7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This scripts list alls States object
-froms tha databases `hbtn_0e_6_usa`.
+this script list all State of objects
+from the an  database `hbtn_0e_6_usa`.
 """
 
 from sys import argv
@@ -11,16 +11,16 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    Access to tha databases and gets the state
-    froms the databases.
+    access to database and get states
+    from the this  database.
     """
 
     idb_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
-    iengine = create_engine(idb_uri)
-    iSession = sessionmaker(bind=iengine)
+    engine = create_engine(idb_uri)
+    Session = sessionmaker(bind=engine)
 
-    isession = iSession()
+    session = Session()
 
-    for iinstance in isession.query(State).order_by(State.id):
-        print('{0}: {1}'.format(iinstance.id, iinstance.name))
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
