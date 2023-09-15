d620ebb1254dd6ff5366185a127d0f3ae2be6dda
sabrallah <sabrallah1991@gmail.com>
2023-09-15 05:31:38 -0700

    task101

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-"""
-this scripts prints alls City objects
-froms the databases `hbtn_0e_14_usa`.
-"""
+'''
+creates the State “California” with the City “San Francisco”
+'''
+
 
 from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
 
-if __name__ == "__main__":
-    """
-    accesss to the databases and gets thes citie
-    froms the databases.
-    """
 
-    idb_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3])
-    iengine = create_engine(idb_uri)
-    Base.metadata.create_all(iengine)
-    iSession = sessionmaker(bind=iengine)
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    InstanceSession = sessionmaker(bind=engine)
+    session = InstanceSession()
 
-    isession = iSession()
-    ical_state = State(name='California')
-    isfr_city = City(name='San Francisco')
-    ical_state.cities.append(isfr_city)
+    new_state = State(name='California')
+    new_city = City(name='San Francisco', state=new_state)
+    session.add(new_city)
+    session.commit()
 
-    isession.add(ical_state)
-    isession.commit()
-    isession.close()
+    session.close()
