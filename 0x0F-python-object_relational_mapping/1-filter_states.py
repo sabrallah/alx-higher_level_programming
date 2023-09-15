0be25eb20e980dc1d76785b098bd266dbf8e4b96
sabrallah <sabrallah1991@gmail.com>
2023-09-15 15:21:30 -0700

    task1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,49 @@
 #!/usr/bin/python3
 """
-Thes scripts list all state with
-a `name` starting with tha letters `N`
-frome tha databases `hbtn_0e_0_usa`.
+Script listing states starting with N from hbtn_0e_0_usa db
 """
 
 import MySQLdb
-from sys import argv
+import sys
+
+
+def main():
+
+    connection = connect_to_db()
+    cursor = connection.cursor()
+
+    # Query to select states starting with N
+    query = ("SELECT * FROM states "
+             "WHERE name LIKE 'N%'")
+
+    cursor.execute(query)
+
+    states = cursor.fetchall()
+
+    print_states(states)
+
+    cursor.close()
+    connection.close()
+
+
+def connect_to_db():
+    """Connect to MySQL database"""
+
+    return MySQLdb.connect(
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        port=3306,
+        host='localhost'
+    )
+
+
+def print_states(states):
+    """Print fetched states"""
+
+    for state in states:
+        print(state)
+
 
 if __name__ == '__main__':
-    """
-    Accesse to tha databases and gets the states
-    froms tha databases.
-    """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
-
-    icur = db.cursor()
-    icur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY 'N%' \
-                 ORDER BY states.id ASC")
-    irows = icur.fetchall()
-
-    for row in irows:
-        print(row)
+    main()
