cf80fb420607d35f82b86a75b96daf1e8e4b9115
sabrallah <sabrallah1991@gmail.com>
2023-09-15 16:52:31 -0700

    task102

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all City objects from the database hbtn_0e_101_usa
+list all the City objects from this database hbtn_0e_101_usa
 """
 import sys
 from relationship_state import Base, State
@@ -14,8 +14,8 @@ if __name__ == '__main__':
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    insSession = sessionmaker(bind=engine)
+    session = insSession()
 
     st = session.query(State).join(City).order_by(City.id).all()
 
