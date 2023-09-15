d550639d279416b619cb49432a433265b2004ea6
sabrallah <sabrallah1991@gmail.com>
2023-09-15 16:24:18 -0700

    task11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-"""
-This scripts adds the States objects
-`Louisiana` to the databases `hbtn_0e_6_usa`.
-"""
+'''
+add the state of object of “Louisiana”
+'''
+
 
 from sys import argv
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
-if __name__ == "__main__":
-    """
-    Access to the databases and gets a states
-    froms the databases.
-    """
 
-    idb_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3])
-    iengine = create_engine(idb_uri)
-    iSession = sessionmaker(bind=iengine)
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    insSession = sessionmaker(bind=engine)
+    session = insSession()
 
-    isession = iSession()
-
-    lou_state = State(name='Louisiana')
-    isession.add(lou_state)
-    isession.commit()
-    print('{0}'.format(lou_state.id))
-    isession.close()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
+    session.close()
