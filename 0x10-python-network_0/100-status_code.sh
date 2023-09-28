8ab9f8e4a8153660a8c8e6116622d12c54e1b10a
sabrallah <sabrallah1991@gmail.com>
2023-09-28 04:58:55 -0700

    task100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # This script checks the HTTP status code of a given URL
+
 URL="https://example.com"  # Replace with the desired URL
+
 # Send a HEAD request to the URL and retrieve the status code
 status_code=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+
 # Check the status code and display the result
 if [ $status_code -eq 100 ]; then
     echo "Status code 100: Continue"
