9a6680b2da9b34174be010ce6da49d62c9e813d0
sabrallah <sabrallah1991@gmail.com>
2023-09-28 05:06:05 -0700

    task100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# This script sends a request to a URL and displays only status code of response
+# This script sends a request to URL and displays only status code of response
 
 curl -s -o /dev/null -w "%{http_code}" "$1"
