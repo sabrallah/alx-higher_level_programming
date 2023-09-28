da6bf923dfdec9fb09bb791df252bf2a1e3d7022
sabrallah <sabrallah1991@gmail.com>
2023-09-28 05:30:14 -0700

    task102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes request 0.0.0.0:5000/catch_me and causes server respond "You got me!"
-curl -sLX PUT -d "user_id=98" -H "Origin: AlxAfrica" 0.0.0.0:5000/catch_me
+curl -sLX PUT -H "Origin: alxafrica" -d "user_id=98" 0.0.0.0:5000/catch_me
