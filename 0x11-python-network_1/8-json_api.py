97c47546ca64a28a2de0509f3b03a3055b93abbf
sabrallah <sabrallah1991@gmail.com>
2023-09-29 03:14:49 -0700

    task8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,11 +14,13 @@ if __name__ == "__main__":
         q = sys.argv[1]
 
     try:
-        response = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+        response = requests.post("http://0.0.0.0:5000/search_user",
+                                 data={"q": q})
         json_response = response.json()
 
         if json_response:
-            print("[{}] {}".format(json_response.get("id"), json_response.get("name")))
+            print("[{}] {}".format(json_response.get("id"),
+                  json_response.get("name")))
         else:
             print("No result")
     except ValueError:
