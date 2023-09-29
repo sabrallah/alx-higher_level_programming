005ee4f6a19df298cde6b373aaf91d8612151c45
sabrallah <sabrallah1991@gmail.com>
2023-09-29 02:00:33 -0700

    task0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script fetches the URL "https://alx-intranet.hbtn.io/status"
+and displays the body response.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode("utf-8"))
