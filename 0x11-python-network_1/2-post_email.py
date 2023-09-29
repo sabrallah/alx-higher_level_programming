0a0d861bbe5f0bb3adf9aaa6d5644aef0aa642bc
sabrallah <sabrallah1991@gmail.com>
2023-09-29 02:20:13 -0700

    task2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-
-import urllib.parse
 import urllib.request
+import urllib.parse
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.exit(1)
+
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data)
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
 
-    with urllib.request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-
-    print("Your email is:", email)
-    print("Response body:", body)
+    with urllib.request.urlopen(url, data=data) as response:
+        print(response.read().decode('utf-8'))
