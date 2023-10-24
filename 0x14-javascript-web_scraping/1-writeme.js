d2c446cc34f96be521c980252a752b494583c0ec
sabrallah <sabrallah1991@gmail.com>
2023-10-24 16:04:54 -0700

    task1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const file = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(file, content, (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+});
